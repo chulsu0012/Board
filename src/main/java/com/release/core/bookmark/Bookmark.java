48f8103f10c981928a9cc03ddeee99c9f2d6db48
[chulsu0012] <[chulsu0012@gmail.com]>
2023-09-17 16:12:38 +0900

    add Bookmark

--- src/main/java/com/release/core/bookmark/Bookmark.java
+++ src/main/java/com/release/core/bookmark/Bookmark.java
@@ -1,4 +1,45 @@
 package com.release.core.bookmark;
 
 public class Bookmark {
+    private int bookmarkId;
+    private int userId;
+    private int postId;
+
+    public Bookmark(int bookmarkId, int userId, int postId) {
+        this.bookmarkId = bookmarkId;
+        this.userId = userId;
+        this.postId = postId;
+    }
+
+    public int getBookmarkId() {
+        return this.bookmarkId;
+    }
+
+    public void setBookmarkId(int bookmarkId) {
+        this.bookmarkId = bookmarkId;
+    }
+
+    public int getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getPostId() {
+        return this.postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    @Override
+    public String toString() {
+        return "Bookmark: " +
+                "bookmakrId=" + bookmarkId +
+                ", userId=" + userId +
+                ", postId=" + postId;
+    }
 }
---
+++ src/main/java/com/release/core/bookmark/SQLBookmarkRepository.java
@@ -0,0 +1,26 @@
+package com.release.core.bookmark;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SQLBookmarkRepository implements BookmarkRepository {
+
+    @Autowired
+    SqlSession SqlSession;
+
+    //등록
+    public void doBookmark(Bookmark bookmark) {
+
+    }
+
+    //조회
+    public void loadBookmark(Bookmark bookmark) {
+
+    }
+
+    //삭제
+    public void deleteBookmark(int bookmarkId, int userId) {
+
+    }
+
+}
