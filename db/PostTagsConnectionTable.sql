860eae8b60ead50da495807d1d8b5a11ef6e55db
chulsu0012 <81545875+chulsu0012@users.noreply.github.com>
2023-09-23 23:28:22 +0900

    error correction

--- db/PostTagsConnectionTable.sql
+++ db/PostTagsConnectionTable.sql
@@ -1,6 +1,6 @@
 create table PostTagsConnectionTable (
     connectionId int not null auto_increment,
-    postId char(20) not null,
+    postId int not null,
     tagId int not null,
     primary key(connectionId),
     foreign key (postId) references PostTable(postId),
