c62b5dd61b2a4338e870bef0aee01ecbba9a6af5
chulsu0012 <81545875+chulsu0012@users.noreply.github.com>
2023-10-03 20:07:22 +0900

    type conversion

--- db/TagTable.sql
+++ db/TagTable.sql
@@ -1,7 +1,7 @@
 create table TagTable(
-    tagId int not null auto_increment,
+    tagId bigint not null auto_increment,
     tagName char(20) not null,
-    categoryId int not null,
+    categoryId bigint not null,
     primary key(tagId),
     foreign key (categoryId) references CategoryTable(categoryId)
 );
