3d5c2f76f13712e792b42f3e217c8207e4b47cee
chulsu0012 <81545875+chulsu0012@users.noreply.github.com>
2023-10-03 20:06:33 +0900

    type conversion

--- db/BookmarkTable.sql
+++ db/BookmarkTable.sql
@@ -1,7 +1,7 @@
 create table BookmarkTable(
-    bookmarkId BIGINT not null auto_increment,
-    userId BIGINT not null,
-    postId BIGINT not null,
+    bookmarkId bigint not null auto_increment,
+    userId bigint not null,
+    postId bigint not null,
     primary key(bookmarkId),
     foreign key(userId) references UserTable(userId),
     foreign key(postId) references postTable(postId)
