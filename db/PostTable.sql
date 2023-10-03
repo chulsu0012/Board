0fc3bfb07562f987bbaa604d4e0591b782b4350c
chulsu0012 <81545875+chulsu0012@users.noreply.github.com>
2023-10-03 20:06:49 +0900

    type conversion

--- db/PostTable.sql
+++ db/PostTable.sql
@@ -1,10 +1,10 @@
 create table PostTable(
-    postId int not null auto_increment,
+    postId bigint not null auto_increment,
     postTitle char(20) not null,
     postDate char(20) not null,
     postContent text not null,
-    postTripDays int not null,
-    writerUserId int not null,
+    postTripDays bigint not null,
+    writerUserId bigint not null,
     primary key(postId),
     foreign key(writerUserId) references UserTable(userId)
 );
