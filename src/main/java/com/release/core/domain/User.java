ad6e3f34d47693000c817827bdc334e41d92c93e
sbyy77 <sbyy77@naver.com>
2023-11-01 18:47:35 +0900

    chore: build.gradle

--- src/main/java/com/release/core/domain/User.java
+++ src/main/java/com/release/core/domain/User.java
@@ -10,18 +10,67 @@ import java.time.LocalDateTime;
 @Getter @Setter
 @Table(name = "UserTable")
 public class User {
-    private Long id;
-    private String name;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false, length = 20, unique = true)
+    private String userName;
+
+    @Column(nullable = false, length = 20)
+    private String userEmail;
+
+    @Column(nullable = false, length = 20)
+    private String userPassword;
+
+    private String userRegisterDate;
+
+
+    @Column(name = "UserIsAdmin")
+    private Long UserIsAdmin;
+
     public Long getId() {
-        return id;
+        return userId;
     }
     public void setId(Long id) {
-        this.id = id;
+        this.userId = id;
     }
     public String getName() {
-        return name;
+        return userName;
     }
     public void setName(String name) {
-        this.name = name;
+        this.userName = name;
     }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserRegisterDate() {
+        return userRegisterDate;
+    }
+
+    public void setUserRegisterDate(String userRegisterDate) {
+        this.userRegisterDate = userRegisterDate;
+    }
+
+    public Long getUserIsAdmin() {
+        return UserIsAdmin;
+    }
+
+    public void setUserIsAdmin(Long userIsAdmin) {
+        UserIsAdmin = userIsAdmin;
+    }
+
 }
