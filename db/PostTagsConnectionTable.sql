0f5c725c442923865800b4782da200f87fd4e590
chulsu0012 <81545875+chulsu0012@users.noreply.github.com>
2023-11-05 21:24:49 +0900

    Update PostTagsConnectionTable.sql

--- db/PostTagsConnectionTable.sql
+++ db/PostTagsConnectionTable.sql
@@ -2,7 +2,5 @@ create table PostTagsConnectionTable (
     connectionId bigint not null auto_increment,
     postId bigint not null,
     tagId bigint not null,
-    primary key(connectionId),
-    foreign key (postId) references PostTable(postId),
-    foreign key (tagId) references TagTable(tagId)
+    primary key(connectionId)
 );
