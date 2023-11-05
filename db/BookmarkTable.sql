fdf79ab9839205b87a8ec22334b641d0f65ca07a
chulsu0012 <81545875+chulsu0012@users.noreply.github.com>
2023-11-05 21:23:56 +0900

    Update BookmarkTable.sql

--- db/BookmarkTable.sql
+++ db/BookmarkTable.sql
@@ -2,7 +2,5 @@ create table BookmarkTable(
     bookmarkId bigint not null auto_increment,
     userId bigint not null,
     postId bigint not null,
-    primary key(bookmarkId),
-    foreign key(userId) references UserTable(userId),
-    foreign key(postId) references postTable(postId)
+    primary key(bookmarkId)
 );
