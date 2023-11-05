a7db242236ee45256555e37dacd9be70a1795fa8
chulsu0012 <81545875+chulsu0012@users.noreply.github.com>
2023-11-05 21:24:26 +0900

    Update PostTable.sql

--- db/PostTable.sql
+++ db/PostTable.sql
@@ -4,7 +4,5 @@ create table PostTable(
     postDate char(20) not null,
     postContent text not null,
     postTripDays bigint not null,
-    writerUserId bigint not null,
-    primary key(postId),
-    foreign key(writerUserId) references UserTable(userId)
+    writerUserId bigint not null
 );
