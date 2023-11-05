62bf26f86900805ab135911648c7e804143a136c
chulsu0012 <81545875+chulsu0012@users.noreply.github.com>
2023-11-05 21:26:29 +0900

    Update TagTable.sql

--- db/TagTable.sql
+++ db/TagTable.sql
@@ -2,6 +2,5 @@ create table TagTable(
     tagId bigint not null auto_increment,
     tagName char(20) not null,
     categoryId bigint not null,
-    primary key(tagId),
-    foreign key (categoryId) references CategoryTable(categoryId)
+    primary key(tagId)
 );
